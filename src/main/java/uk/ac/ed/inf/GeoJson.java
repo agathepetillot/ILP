ddf0c4515f28a65e6093ef56cbf87c92381ee24f
agathepetillot <80222611+agathepetillot@users.noreply.github.com>
2021-11-07 18:27:53 +0000

    Add files via upload

--- src/main/java/uk/ac/ed/inf/GeoJson.java
+++ src/main/java/uk/ac/ed/inf/GeoJson.java
@@ -11,19 +11,27 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 public class GeoJson {
 
     private static final HttpClient client = HttpClient.newHttpClient();
     public static HttpResponse<String> response;
-    public static List<Polygon> g = new ArrayList<Polygon>();
+    public static List<Polygon> NoFlyZone = new ArrayList<Polygon>();
+    public static List<Point> Landmarks = new ArrayList<Point>();
+    
+    public GeoJson()
+    {
+        NoFlyZone("localhost", "9898");
+        Landmarks("localhost", "9898");
+        System.out.println(Landmarks);
+    }
 
-
-    public static List<Polygon> NoFlyZone(String machine_name, String web_server_port) {
+     public static void Landmarks(String machine_name, String web_server_port) {
 
         //Make a request so that we can get a response
         //HttpRequest assumes that it is a GET request by default
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://" + machine_name + ":" + web_server_port + "/buildings/no-fly-zones.geojson")).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://" + machine_name + ":" + web_server_port + "/buildings/landmarks.geojson")).build();
         //Call the send method on the client created
 
         try {
@@ -43,10 +51,38 @@ public class GeoJson {
             List<Feature> f = fc.features();
 
             for (Feature i : f) {
-                g.add((Polygon) i.geometry());
+                Landmarks.add((Point) i.geometry());
             }
         }
-        return (g);
+    }
+
+    public static void NoFlyZone(String machine_name, String web_server_port) {
+
+        //Make a request so that we can get a response
+        //HttpRequest assumes that it is a GET request by default
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://" + machine_name + ":" + web_server_port + "/buildings/no-fly-zones.geojson")).build();
+        //Call the send method on the client created
+
+        try {
+
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+           
+        } catch (IOException | InterruptedException ex) {
+
+            System.out.println("Cannot retrieve data from server: check server name and query");
+        }
+
+        if (response.statusCode() != 200) {
+            System.exit(1);
+        } else {
+
+            FeatureCollection fc = FeatureCollection.fromJson(response.body());
+            List<Feature> f = fc.features();
+
+            for (Feature i : f) {
+                NoFlyZone.add((Polygon) i.geometry());
+            }
+        }
     }
 
     public static boolean point_intersects(Double endPosLng, Double endPosLat, List<Polygon> polygons) {
@@ -60,38 +96,45 @@ public class GeoJson {
         return intersects;
     }
 
-    public static boolean line_intersects(List<Polygon> polygons) {
+    public static boolean line_intersects(Line2D line, List<Polygon> polygons) {
 
-//        boolean intersects = false;
-        for (Polygon poly : polygons) {
-            System.out.println(poly.toJson());
-        }//{
-//                intersects = true;
-//            }
-//        for (int i = 0; i < poly.npoints; i++) {
-//            int nextI = (i + 1) % poly.npoints;
-//            Line2D edge = new Line2D.Double(poly.xpoints[i], poly.ypoints[i], poly.xpoints[nextI], poly.ypoints[nextI]);
-//            if (line.intersectsLine(edge)) {
-//                intersects = true;
-//            }
-//        }
+        boolean intersects = false;
+        for (Polygon poly : polygons) 
+        {
+            List<List<Point>> coords = poly.coordinates();
+            for (int i = 0; i < coords.size(); i++) // We are analysing the polygons one at a time
+            {
+                for (int j = 0; j < coords.get(i).size()-1; j++)
+                {
+                    Line2D edge = new Line2D.Double(coords.get(i).get(j).coordinates().get(0), coords.get(i).get(j).coordinates().get(1), coords.get(i).get(j+1).coordinates().get(0), coords.get(i).get(j+1).coordinates().get(1));
+                    
+                    if (line.intersectsLine(edge)){
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
-//    public static void GoToNextPosition(ArrayList<Double> startPoint, ArrayList<Double> endPoint, double increment) {
-//
-//
-//        LongLat start_LongLat = new LongLat(startPoint.get(0), startPoint.get(1));
-//        LongLat end_LongLat = new LongLat(endPoint.get(0), endPoint.get(1));
-//
-////        if line_intersects(start_LongLat, end_LongLat, LongLat.MOVE_LENGTH){
-////          go to closest way point (check for intersection again)(use line_intersects())
-////            GoToNextPosition(new positions);
-////        }
-////        else{
-//////          go to the end point using 10 degree increments and checking whether or not these 10 degree increments intersect at any point
-////            with a polygon.
-////        }
+    public static void GoToTarget(LongLat startPoint, LongLat endPoint) {
+
+        Line2D line = new Line2D.Double(startPoint.longitude,startPoint.latitude,endPoint.longitude,endPoint.latitude);
+        
+//        if (line_intersects(line, GeoJson.NoFlyZone))
+//        {
+//            FindNearestWaypoint();
+//            if(line_intersects())
+//            
+//        }
+//        
+//          go to closest way point (check for intersection again)(use line_intersects())
+//            GoToNextPosition(new positions);
+//        }
+//        else{
+////          go to the end point using 10 degree increments and checking whether or not these 10 degree increments intersect at any point
+//            with a polygon.
+//        }
 //
 //
 //        double distanceTo = start_LongLat.distanceTo(end_LongLat);
@@ -105,31 +148,28 @@ public class GeoJson {
 //            start_LongLat.nextPosition((int) ((angle / 10) * 10));
 //
 //        }
-//    }
+    }
 
 
         public static void main (String[]args){
 
             LongLat pos = new LongLat(-3.1904, 55.9441);
             LongLat nextPos = pos.nextPosition(90);
-            System.out.println(point_intersects(pos.longitude, pos.latitude, NoFlyZone("localhost", "9898")));
-            System.out.println(point_intersects(nextPos.longitude, nextPos.latitude, NoFlyZone("localhost", "9898")));
+            System.out.println(point_intersects(pos.longitude, pos.latitude, GeoJson.NoFlyZone));
+            System.out.println(point_intersects(nextPos.longitude, nextPos.latitude, GeoJson.NoFlyZone));
 
 //            LongLat pos1 = new LongLat(-3.1893, 55.9452);
 //            LongLat pos2 = new LongLat(-3.1902, 55.9456);
+            
 
-            List<Polygon> poly = (GeoJson.NoFlyZone("localhost", "9898"));
+            Line2D[] lines = new Line2D[]{
+                new Line2D.Double(-3.1893,55.9452, -3.1902, 55.9456), //true
+                new Line2D.Double(-3.1893, 55.9452, -3.1870, 55.9457) //false
+            };
 
-            line_intersects(poly);
-
-//            Line2D[] lines = new Line2D[]{
-//                    new Line2D.Double(55.9452, -3.1893, 55.9456, -3.1902), //true
-//                    new Line2D.Double(55.9452, -3.1893, 55.9457, -3.1870) //false
-//            };
-//
-//            for (Line2D line : lines) {
-//                System.out.printf("%s: %b\n", line, line_intersects(line, (java.awt.Polygon) poly));
-//            }
+            for (Line2D line : lines) {
+                System.out.printf("%s: %b\n", line, line_intersects(line, NoFlyZone));
+            }
 
         }
 }
